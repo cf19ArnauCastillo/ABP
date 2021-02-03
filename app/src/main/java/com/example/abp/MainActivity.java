fd221f39678182f76c0a4f7f38dde7092895523d
Adam1047 <58786408+Adam1047@users.noreply.github.com>
2021-02-03 20:05:57 +0100

    Adam

--- app/src/main/java/com/example/abp/MainActivity.java
+++ app/src/main/java/com/example/abp/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.abp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    firebaseAuth = FirebaseAuth.getInstance();
+    firebaseAuthListener = new FirebaseAuth.AuthStateListener(){
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (firebaseAuthListener != null){
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+        }
     }
 }
