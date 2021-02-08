8a98a2b7e417cfc395663e39d469f776d6e0eb66
cf19adan.montesinos <cf19adan.montesinos>
2021-02-08 18:52:29 +0100

    Adam

--- app/src/main/java/com/example/abp/MainActivity.java
+++ app/src/main/java/com/example/abp/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.abp;
 
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -16,7 +17,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,6 +24,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class GoogleSignInActivity extends AppCompatActivity {
+
     Button signInButton;
 
     private static final String TAG = "GoogleActivity";
@@ -110,8 +111,5 @@ public class GoogleSignInActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Perfil.class);
         startActivity(intent);
     }
-
-}
-
 
 }
