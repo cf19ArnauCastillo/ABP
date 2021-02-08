a59c75ab2edb13c2be3f45099c925181d61ab0f5
Cristian Montañés <cf19cristian.montanes@iesjoandaustria.org>
2021-02-08 20:18:54 +0100

    Slide Menu

---
+++ app/src/main/java/com/example/abp/Menu_desplegable.java
@@ -0,0 +1,100 @@
+package com.example.abp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class Menu_desplegable extends AppCompatActivity {
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_desplegable);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, "Open navigation drawer", "Close navigation drawer");
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.contenedor, new Chat()).commit();
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_desplegable, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationSelected(MenuItem item){
+        int id = item.getItemId();
+
+        FragmentManager fragmentManager =getSupportFragmentManager();
+
+        if(id == R.id.quedadas){
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new Quedadas()).commit();
+        }
+        if(id == R.id.mapa){
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new Mapa()).commit();
+        }
+        if(id == R.id.chat){
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new Chat()).commit();
+        }
+        if(id == R.id.perfil){
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new Perfil()).commit();
+        }
+        if(id == R.id.config){
+            fragmentManager.beginTransaction().replace(R.id.contenedor, new Configuracion()).commit();
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
