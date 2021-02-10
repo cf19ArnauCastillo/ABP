ff6771022c26439fbf0ecc3caca28ae676d9e5ad
Cristian Montañés <cf19cristian.montanes@iesjoandaustria.org>
2021-02-10 18:17:20 +0100

    Update Menu_desplegable.java

--- app/src/main/java/com/example/abp/Menu_desplegable.java
+++ app/src/main/java/com/example/abp/Menu_desplegable.java
@@ -12,16 +12,11 @@ import com.google.android.material.navigation.NavigationView;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.FragmentManager;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class Menu_desplegable extends AppCompatActivity {
-    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,7 +71,7 @@ public class Menu_desplegable extends AppCompatActivity {
     public boolean onNavigationSelected(MenuItem item){
         int id = item.getItemId();
 
-        FragmentManager fragmentManager =getSupportFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
         if(id == R.id.quedadas){
             fragmentManager.beginTransaction().replace(R.id.contenedor, new Quedadas()).commit();
