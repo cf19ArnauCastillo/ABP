47e603de14994a518eeca9325166d9a2a7f57a65
Cristian Montañés <cf19cristian.montanes@iesjoandaustria.org>
2021-03-28 18:36:09 +0200

    Update Quedadas.java

--- app/src/main/java/com/example/abp/FragmentQuedadas/Quedadas.java
+++ app/src/main/java/com/example/abp/FragmentQuedadas/Quedadas.java
@@ -1,6 +1,7 @@
 package com.example.abp.FragmentQuedadas;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -32,29 +33,30 @@ public class Quedadas extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_quedadas,container,false);
         mRecyclerView = mRecyclerView.findViewById(R.id.recyclerviewQuedadas);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         lista = new ArrayList<Quedada>();
 
-        Button btnSearch = findViewById(R.id.BotonCrearQuedadas);
+        Button btnSearch = v.findViewById(R.id.BotonCrearQuedadas);
         btnSearch.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
                 alert.setTitle("Añade quedada");
 
-                final EditText input = new EditText(this);
+                final EditText input = new EditText(getContext());
                 input.setHint("Nombre de aficion");
                 alert.setView(input);
 
-                final EditText input = new EditText(this);
-                input.setHint("Horario");
-                alert.setView(input);
+                final EditText input2 = new EditText(getContext());
+                input2.setHint("Horario");
+                alert.setView(input2);
 
                 alert.setPositiveButton("Confirma", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
-                        String movie = input.getText().toString();
-                        fetchData process = new fetchData(text,this);
-                        process.execute();
+                        String aficion = input.getText().toString();
+                        String horario = input2.getText().toString();
+
                     }
                 });
 
@@ -65,31 +67,31 @@ public class Quedadas extends Fragment {
                 });
                 alert.show();
             }
+            });
 
-        ValueEventListener valueEventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                lista.clear();
-                if (dataSnapshot.exists()) {
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        Quedada quedada = snapshot.getValue(Quedada.class);
-                        lista.add(quedada);
+            ValueEventListener valueEventListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    lista.clear();
+                    if (dataSnapshot.exists()) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            Quedada quedada = snapshot.getValue(Quedada.class);
+                            lista.add(quedada);
+                        }
                     }
                 }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
 
-            }
-        };
-        RefQuedadas = (DatabaseReference) FirebaseDatabase.getInstance().getReference("Quedadas")
-                .orderByChild("id");
+                }
+            };
+            RefQuedadas =(DatabaseReference)FirebaseDatabase.getInstance().getReference("Quedadas").orderByChild("id");
         RefQuedadas.addListenerForSingleValueEvent(valueEventListener);
 
-        QuedadasAdapter adapter = new QuedadasAdapter(lista);
+            QuedadasAdapter adapter = new QuedadasAdapter(lista);
         mRecyclerView.setAdapter(adapter);
 
-        return inflater.inflate(R.layout.fragment_quedadas, container, false);
+        return v;
     }
 }
